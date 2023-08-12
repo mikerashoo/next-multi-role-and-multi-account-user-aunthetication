66f12ae5b9ea35e730114f5c198efba3475abfe3
Mikiyas Birhanu <37136032+mikerashoo@users.noreply.github.com>
2023-08-13 00:01:35 +0300

    Reusable component issue fix

--- components/elemtents/buttons/BtnProps.tsx
+++ components/elemtents/buttons/BtnProps.tsx
@@ -2,14 +2,15 @@ import { LinkProps } from "next/link";
 
 export interface BtnProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    children: React.ReactNode[];
+    children?: JSX.Element | JSX.Element[];
+
     icon?: React.ReactElement;
     isLoading?: boolean;
 }
 
 export interface NavBtnProps extends LinkProps {
     href: string;
-    children?: React.ReactNode[];
+    children?: JSX.Element | JSX.Element[];
 
     icon?: React.ReactElement;
 }

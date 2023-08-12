c6b45e2655d0ef0acb761e3ff9b6ac8dfe1cb111
Mikiyas Birhanu <37136032+mikerashoo@users.noreply.github.com>
2023-08-13 00:22:10 +0300

    reusable card issue fix on production

--- components/elemtents/cards/props.tsx
+++ components/elemtents/cards/props.tsx
@@ -1,6 +1,11 @@
-import { PropsWithChildren } from "react";
+import { HTMLAttributes, ReactNode } from "react";
 
-export interface CardProps extends PropsWithChildren {
+export interface CardProps
+    extends React.DetailedHTMLProps<
+        HTMLAttributes<HTMLDivElement>,
+        HTMLDivElement
+    > {
+    children?: JSX.Element;
     title?: string;
     error?: string;
 }

34828936072a1f1af19faa751339bee88b202734
Mikiyas Birhanu <37136032+mikerashoo@users.noreply.github.com>
2023-08-13 00:14:01 +0300

    Default card issue fix

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,4 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 import { LoginUserForm } from "~/components/auth/LoginUserForm";
-import { nextAuthOptions } from "~/shared/nextAuthOptions";
-import { DefaultCard } from "~/components/elemtents/cards";
 import { AccountType } from "~/utils/constants/userRoles";
 
 export default async function Page() {
--- components/auth/UserCard.tsx
+++ components/auth/UserCard.tsx
@@ -65,57 +65,61 @@ function UserCard(props: { user: User | undefined; account: Account }) {
     };
     return (
         <DefaultCard error={error}>
-            <p>Account Name: {account?.name}</p>
-            <Divider />
             <div>
-                <WarningButton onClick={() => setIsModalOpen(true)}>
-                    Delete my {account.type} account
-                </WarningButton>
+                <p>Account Name: {account?.name}</p>
+                <Divider />
+                <div>
+                    <WarningButton onClick={() => setIsModalOpen(true)}>
+                        Delete my {account.type} account
+                    </WarningButton>
 
-                {/* Put this part before </body> tag */}
-                <input
-                    type="checkbox"
-                    id="delete_account_modal"
-                    className="modal-toggle"
-                />
-                <div className={`modal ${isModalOpen ? "modal-open" : ""}`}>
-                    <div className="modal-box">
-                        <h3 className="font-bold text-lg">Delete Account!</h3>
-                        <p className="py-4">
-                            Are you sure you want to delete {account.type}{" "}
-                            account
-                        </p>
+                    {/* Put this part before </body> tag */}
+                    <input
+                        type="checkbox"
+                        id="delete_account_modal"
+                        className="modal-toggle"
+                    />
+                    <div className={`modal ${isModalOpen ? "modal-open" : ""}`}>
+                        <div className="modal-box">
+                            <h3 className="font-bold text-lg">
+                                Delete Account!
+                            </h3>
+                            <p className="py-4">
+                                Are you sure you want to delete {account.type}{" "}
+                                account
+                            </p>
 
-                        <form
-                            onSubmit={handleSubmit(onSubmit)}
-                            className="space-y-2"
-                        >
-                            <input
-                                value={user?.userId}
-                                type="hidden"
-                                {...register("userId")}
-                            />
+                            <form
+                                onSubmit={handleSubmit(onSubmit)}
+                                className="space-y-2"
+                            >
+                                <input
+                                    value={user?.userId}
+                                    type="hidden"
+                                    {...register("userId")}
+                                />
 
-                            <input
-                                value={account.id?.toString() ?? ""}
-                                type="hidden"
-                                {...register("accountId")}
-                            />
-                            <div className="flex items-end justify-end align-center">
-                                <DefaultButton
-                                    type="button"
-                                    onClick={() => setIsModalOpen(false)}
-                                >
-                                    No Cancel!
-                                </DefaultButton>
-                                <WarningButton
-                                    isLoading={isLoading}
-                                    type="submit"
-                                >
-                                    Yes Delete!
-                                </WarningButton>
-                            </div>
-                        </form>
+                                <input
+                                    value={account.id?.toString() ?? ""}
+                                    type="hidden"
+                                    {...register("accountId")}
+                                />
+                                <div className="flex items-end justify-end align-center">
+                                    <DefaultButton
+                                        type="button"
+                                        onClick={() => setIsModalOpen(false)}
+                                    >
+                                        No Cancel!
+                                    </DefaultButton>
+                                    <WarningButton
+                                        isLoading={isLoading}
+                                        type="submit"
+                                    >
+                                        Yes Delete!
+                                    </WarningButton>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
